0bec2c785a9ccf9cbe2f9dff96295416a29b4a40
Seowon Kim <58676931+swkim-sm@users.noreply.github.com>
2021-06-23 13:49:30 +0900

    Create src.cpp

---
+++ codingTest/Programmers/타겟넘버/src.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int answer = 0;
+void dfs(vector<int> numbers, int target, int sum, int count) {
+    if(count == 1) {
+        if(sum == target) answer++;
+        return;
+    }
+    dfs(numbers, target, sum+numbers[count], count-1);
+    dfs(numbers, target, sum-numbers[count], count-1);
+}
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, numbers.size());
+    return answer;
+}
